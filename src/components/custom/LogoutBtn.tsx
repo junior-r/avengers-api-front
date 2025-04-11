8fb4badb274d60eff53c9c6da00dde90e4ffb96d
junior31064049@gmail.com <junior31064049@gmail.com>
2025-04-11 18:10:53 -0500

    Add new logout button interface

--- src/components/custom/LogoutBtn.tsx
+++ src/components/custom/LogoutBtn.tsx
@@ -1,24 +1,40 @@
 import { useAuthStore } from '@/store/auth/useAuthStore'
 import { logout } from '@/actions/auth/logout'
+import { LogOutIcon } from 'lucide-react'
+import { Button } from '../ui/button'
 
-function LogoutBtn() {
-    const user = useAuthStore(state => state.user)
-    const setUser = useAuthStore(state => state.setUser)
+type Props = {
+  showIcon?: boolean
+}
 
-    const handleEvent = async () => {
-        const res = await logout()
-        if (res.status === 200) {
-            setUser(null)
-            return
-        }
-        return
+function LogoutBtn({ showIcon = false }: Props) {
+  const user = useAuthStore((state) => state.user)
+  const setUser = useAuthStore((state) => state.setUser)
+
+  const handleEvent = async () => {
+    const res = await logout()
+    if (res.status === 200) {
+      setUser(null)
+      return
     }
+    return
+  }
 
-    if (user) {
-        return (
-            <button onClick={handleEvent}>Logout</button>
-        )
+  if (user) {
+    if (showIcon) {
+      return (
+        <Button variant="outline" size="icon" onClick={handleEvent} title="Logout">
+          <LogOutIcon />
+        </Button>
+      )
+    } else {
+      return (
+        <button onClick={handleEvent} title="Logout">
+          Logout
+        </button>
+      )
     }
+  }
 }
 
 export default LogoutBtn
