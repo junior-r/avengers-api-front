0b1916869b12a4f97071ca500ff09cc0959a18e5
junior31064049@gmail.com <junior31064049@gmail.com>
2025-04-11 18:14:17 -0500

    max method added to fields name and lastname

--- src/schemas/auth/index.ts
+++ src/schemas/auth/index.ts
@@ -6,8 +6,8 @@ export const LoginSchema = z.object({
 })
 
 export const RegisterSchema = LoginSchema.extend({
-    name: z.string().min(1, { message: "Name is required" }),
-    lastName: z.string().min(1, { message: "Last name is required" }),
+    name: z.string().min(2, { message: "Name is required" }).max(20),
+    lastName: z.string().min(2, { message: "Last name is required" }).max(20),
     confirmPassword: z.string().min(6, { message: "Password must be at least 6 characters long" })
 }).refine(data => data.password === data.confirmPassword, {
     message: "Passwords do not match",
